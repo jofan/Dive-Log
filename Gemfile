f474fbf1ddb9585b007139168ca80875659c9567
Stefan Lidén <stefan.liden@gmail.com>
2010-09-22 10:42:57 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+gem 'mongoid', '2.0.0.beta.18'
+gem 'bson_ext', '1.0.4'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'haml', '3.0.18'
+gem 'compass', '>= 0.10.5'
+gem 'gravatar_image_tag', '0.1.0'
+
+group :development do
+  gem 'rspec-rails', '2.0.0.beta.20'
+  gem 'annotate-models', '1.0.4'
+end
+
+group :test do
+  gem 'rspec', '2.0.0.beta.20'
+  gem 'factory_girl', '1.3.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Divelog::Application.config.secret_token = 'ab09a17e207aeab6ea98bec71e56441514a0f282e32ccb7dd7279a4e717e30d824fbf5935bda6c807dc94a08717dfe212e436d15d834aa8cec8701acc165eb9b'
